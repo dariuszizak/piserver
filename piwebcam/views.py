d339cd47f9b2dd7bd0686ce993d632a87dc58c2f
dariuszizak <dariusz.izak@gmail.com>
2018-03-28 21:26:14 +0000

    FIX:Proper use of picamera module

--- piwebcam/views.py
+++ piwebcam/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import picamera
 
+camera = picamera.PiCamera()
 
 def index(request):
+    camera.capture("piwebcam/static/piwebcam/image.jpg")
     return render(request,
                   "piwebcam/index.html.jj2")
